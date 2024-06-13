87c4d7b93f1792c19d40fc86aceb280ff2f62c75
Gabriela <64268942+gchipanap@users.noreply.github.com>
2024-06-13 15:03:30 -0500

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -4,13 +4,11 @@ from bs4 import BeautifulSoup
 def get_zip_links(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Verificar si hay errores en la solicitud
+        response.raise_for_status()
 
         soup = BeautifulSoup(response.content, 'html.parser')
-        # Imprimir toda la estructura HTML para analizar su contenido
         print(soup.prettify())
 
-        # Extraer todos los enlaces que terminan en .zip
         links = [a['href'] for a in soup.find_all('a', href=True) if a['href'].endswith('.zip')]
         full_links = ["https://s3.amazonaws.com/tripdata/" + link for link in links]
         print("Extracted links:", full_links)
@@ -19,9 +17,7 @@ def get_zip_links(url):
         print(f"Failed to fetch links from {url}: {e}")
         return []
 
-# URL base
 base_url = 'https://s3.amazonaws.com/tripdata/index.html'
 
-# Obtener todos los enlaces a archivos ZIP
 zip_links = get_zip_links(base_url)
-print(zip_links)  # Mostrar los enlaces obtenidos para verificar
+print(zip_links) 
